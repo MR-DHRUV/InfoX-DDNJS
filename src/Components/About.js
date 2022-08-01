cbdf08c94815acfd2ea57a95b722003bc2f7bdfe
MR-DHRUV <dhruvgupta742@gmail.com>
2022-08-01 17:07:00 +0530

    UI fixes

--- src/Components/About.js
+++ src/Components/About.js
@@ -199,7 +199,7 @@ const About = () => {
                             <div className="container">
                                 {/* Main Title */}
                                 <div className="neoh_fn_title">
-                                    <h3 className="fn_title atmosphere">Our Team Members</h3>
+                                    <h3 className="fn_title atmosphere theme">Our Team Members</h3>
                                     <div className="line">
                                         <span />
                                     </div>
@@ -214,7 +214,39 @@ const About = () => {
                                 </div>
                                 {/* Team List Shortcode */}
                                 <div className="neoh_fn_team">
-                                    <ul className="team_list">
+                                    <ul className="team_list justify-content-center">
+                                        <li className="team_item">
+                                            <div className="t_item">
+                                                <div className="person_info">
+                                                    <div className="img_holder">
+                                                        <img src={require("./Assets/img/author/dhruvaeron.jpeg")} alt="" />
+                                                    </div>
+                                                    <div className="title_holder">
+                                                        <h3 className="fn_title theme">Dhruv Aeron</h3>
+                                                        <p className="fn_desc">Web Lead</p>
+                                                    </div>
+                                                </div>
+                                                <div className="person_social">
+                                                    <ul>
+                                                        <li>
+                                                            <a href="https://www.linkedin.com/in/dhruv-gupta-55034a228/" target='_blank' rel='noreferrer'>
+                                                                <i className="fa-brands fa-linkedin" />
+                                                            </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="https://instagram.com/dhruv._0810" target='_blank' rel='noreferrer'>
+                                                                <i className="fa-brands fa-instagram" />
+                                                            </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="https://github.com/MR-DHRUV " target='_blank' rel='noreferrer'>
+                                                                <i className="fa-brands fa-github" />
+                                                            </a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </li>
                                         <li className="team_item">
                                             <div className="t_item">
                                                 <div className="person_info">
@@ -222,7 +254,7 @@ const About = () => {
                                                         <img src={require("./Assets/img/author/dhruvgupta.png")} alt="" />
                                                     </div>
                                                     <div className="title_holder">
-                                                        <h3 className="fn_title">Dhruv Gupta</h3>
+                                                        <h3 className="fn_title theme">Dhruv Gupta</h3>
                                                         <p className="fn_desc">Web Lead</p>
                                                     </div>
                                                 </div>
@@ -251,10 +283,10 @@ const About = () => {
                                             <div className="t_item">
                                                 <div className="person_info">
                                                     <div className="img_holder">
-                                                        <img src={require("./Assets/img/author/dhruvgupta.png")} alt="" />
+                                                        <img src={require("./Assets/img/author/kartit.jpeg")} alt="" />
                                                     </div>
                                                     <div className="title_holder">
-                                                        <h3 className="fn_title">Dhruv Gupta</h3>
+                                                        <h3 className="fn_title theme">Kartik singh</h3>
                                                         <p className="fn_desc">Web Lead</p>
                                                     </div>
                                                 </div>
@@ -283,10 +315,10 @@ const About = () => {
                                             <div className="t_item">
                                             <div className="person_info">
                                                     <div className="img_holder">
-                                                        <img src={require("./Assets/img/author/dhruvgupta.png")} alt="" />
+                                                        <img src={require("./Assets/img/author/Nishant.jpeg")} alt="" />
                                                     </div>
                                                     <div className="title_holder">
-                                                        <h3 className="fn_title">Dhruv Gupta</h3>
+                                                        <h3 className="fn_title theme">Nishant ola</h3>
                                                         <p className="fn_desc">Web Lead</p>
                                                     </div>
                                                 </div>
@@ -315,74 +347,10 @@ const About = () => {
                                             <div className="t_item">
                                             <div className="person_info">
                                                     <div className="img_holder">
-                                                        <img src={require("./Assets/img/author/dhruvgupta.png")} alt="" />
+                                                        <img src={require("./Assets/img/author/shivesh.jpg")} alt="" />
                                                     </div>
                                                     <div className="title_holder">
-                                                        <h3 className="fn_title">Nishant Ola</h3>
-                                                        <p className="fn_desc">Web Lead</p>
-                                                    </div>
-                                                </div>
-                                                <div className="person_social">
-                                                    <ul>
-                                                        <li>
-                                                            <a href="https://www.linkedin.com/in/dhruv-gupta-55034a228/" target='_blank' rel='noreferrer'>
-                                                                <i className="fa-brands fa-linkedin" />
-                                                            </a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="https://instagram.com/dhruv._0810" target='_blank' rel='noreferrer'>
-                                                                <i className="fa-brands fa-instagram" />
-                                                            </a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="https://github.com/MR-DHRUV " target='_blank' rel='noreferrer'>
-                                                                <i className="fa-brands fa-github" />
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li className="team_item">
-                                            <div className="t_item">
-                                            <div className="person_info">
-                                                    <div className="img_holder">
-                                                        <img src={require("./Assets/img/author/dhruvgupta.png")} alt="" />
-                                                    </div>
-                                                    <div className="title_holder">
-                                                        <h3 className="fn_title">Dhruv Gupta</h3>
-                                                        <p className="fn_desc">Web Lead</p>
-                                                    </div>
-                                                </div>
-                                                <div className="person_social">
-                                                    <ul>
-                                                        <li>
-                                                            <a href="https://www.linkedin.com/in/dhruv-gupta-55034a228/" target='_blank' rel='noreferrer'>
-                                                                <i className="fa-brands fa-linkedin" />
-                                                            </a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="https://instagram.com/dhruv._0810" target='_blank' rel='noreferrer'>
-                                                                <i className="fa-brands fa-instagram" />
-                                                            </a>
-                                                        </li>
-                                                        <li>
-                                                            <a href="https://github.com/MR-DHRUV " target='_blank' rel='noreferrer'>
-                                                                <i className="fa-brands fa-github" />
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li className="team_item">
-                                            <div className="t_item">
-                                            <div className="person_info">
-                                                    <div className="img_holder">
-                                                        <img src={require("./Assets/img/author/dhruvgupta.png")} alt="" />
-                                                    </div>
-                                                    <div className="title_holder">
-                                                        <h3 className="fn_title">Dhruv Gupta</h3>
+                                                        <h3 className="fn_title theme">Shivesh Nandan</h3>
                                                         <p className="fn_desc">Web Lead</p>
                                                     </div>
                                                 </div>
